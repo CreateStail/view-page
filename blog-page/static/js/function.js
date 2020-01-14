881ad93c933f9a25e049a76a4c1b612a50c9ae4d
issuser <mx18801736833@163.com>
2020-01-14 17:15:10 +0800

    1.14

---
+++ blog-page/static/js/function.js
@@ -0,0 +1,53 @@
+$(function() {
+	$.ajaxSetup({
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader('token', window.localStorage.getItem('token'));
+		},
+		complete: function(xhr) {
+			if(xhr.responseJSON.code === -1 && xhr.responseJSON.msg === '请登录') {
+				window.location.href = "login.html";
+			};
+		}
+	});
+})
+
+function getQueryVariable(variable) {
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for(var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split("=");
+		if(pair[0] == variable) {
+			return pair[1];
+		}
+	}
+	return(false);
+}
+
+function checkRequired(param) {
+	for(var key in param) {
+		if($('#' + key).hasClass('Required') && $('#' + key).val().trim() == '') {
+			$('#checkForm').show();
+			return;
+		} else {
+			$('#checkForm').hide();
+		}
+	}
+}
+
+function propSuccess(res, callback) {
+	layer.msg(res.msg, {
+		offset: 'rb',
+		icon: 1,
+		area: ['270px', '70px'],
+		time: 3000
+	}, callback)
+}
+
+function propFailReload(res, callback) {
+	layer.msg(res.msg, {
+			offset: 'rb',
+			icon: 2,
+			area: ['270px', '70px'],
+			time: 3000
+	},callback)
+}
